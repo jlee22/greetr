80453f70eab40f30a76822e1fda0f74e4eff4115
Jeffrey <jfflee.22@gmail.com>
2016-12-19 12:49:39 -0800

    add greeting, formal greeting, greet, log, setLang function

--- Greetr.js
+++ Greetr.js
@@ -22,7 +22,61 @@
     es: 'Inició sesión'
   }
 
-  Greetr.prototype = {};
+  Greetr.prototype = {
+
+    fullName: function() {
+      return this.firstName + ' ' + this.lastName;
+    },
+
+    validate: function() {
+      if (supportedLangs.indexOf(this.language) === -1) {
+        thriow "Invalid language"
+      }
+    }
+
+    greeting: function(){
+      return greetings[this.language] + ' ' + this.firstName + '!';
+    },
+
+    formalGreeting: function() {
+      return formalGreetings[this.language] + ', ' + this.fullName
+    },
+
+    greet: function(formal) {
+      var msg;
+
+      if (formal) {
+        msg = this.formalGreeting();
+      }
+
+      else {
+        msg = this.greeting()
+      }
+
+      if (console) {
+        console.log(msg);
+      }
+      // make chainable
+      return this;
+    },
+
+    log: function() {
+      if (console) {
+        console.log(logMessages[this.language] + ': ' + this.fullName());
+      }
+
+      return this;
+    },
+
+    setLang: function(lang) {
+      this.language = lang;
+
+      this.validate();
+
+      return this;
+    }
+
+  };
 
   Greetr.init = function(firstName, lastName, language) {
 
